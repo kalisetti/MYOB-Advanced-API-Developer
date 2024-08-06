233119dc9cbe2a2a79ac7f4e739f1d1617565762
Siva Kalisetti <siva@alphabiz.com.au>
2024-08-06 11:33:27 +0800

    Example 2.2.3: Using the Put() Method to Retrieve Data from an Inquiry (Contract-Based SOAP)

--- MyBIIntegration/Program.cs
+++ MyBIIntegration/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MyBIIntegration.Default;
 using MyBIIntegration.Integration;
+using MyBIIntegration.ItemAvailabilityData;
 
 namespace MyBIIntegration {
 	internal class Program {
@@ -24,7 +25,6 @@ namespace MyBIIntegration {
 					//Retrieving the list of customers with contacts
 					Console.WriteLine("Retrieving...");
 					InitialDataRetrieval.RetrieveListOfCustomers(soapClient);
-					Console.ReadLine();
 				}
 				catch (Exception e) {
 					Console.WriteLine(e);
@@ -32,12 +32,46 @@ namespace MyBIIntegration {
 					Console.WriteLine("Press any key to continue");
 					Console.ReadLine();
 				}
-				finally { 
+				finally {
 					//Sign out from MYOB Advanced ERP
+					Console.WriteLine("Logout...");
 					soapClient.Logout();
+					Console.WriteLine("Press any key to exit!");
+					Console.ReadLine();
 				}
-				
+
 			}
+
+			//Using the ItemAvailabilityData/0001 endpoint
+			using (ItemAvailabilityData.DefaultSoapClient soapClient = new ItemAvailabilityData.DefaultSoapClient()) {
+				//Sign in to MYOB Advanced ERP
+				soapClient.Login(
+					Properties.Settings.Default.Username,
+					Properties.Settings.Default.Password,
+					Properties.Settings.Default.Tenant,
+					Properties.Settings.Default.Company,
+					null
+				);
+
+				try {
+					//Retrieving the quantities of stock items
+					InitialDataRetrievalGI.RetrieveItemQuantities(soapClient);
+				}
+				catch (Exception e) { 
+					Console.WriteLine(e);
+					Console.WriteLine();
+					Console.WriteLine("Press any key to continue");
+					Console.ReadLine();
+				} finally {
+					//Sign out from MYOB Advanced ERP
+					Console.WriteLine("Logout...");
+					soapClient.Logout();
+					Console.WriteLine("Press any key to exit!");
+					Console.ReadLine();
+				}
+			}
+			Console.ReadLine();
+
 		}
 	}
 }
