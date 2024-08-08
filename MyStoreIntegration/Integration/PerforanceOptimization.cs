238fa1a246deb3bbac6cc06ca585c275afbc4558
Siva Kalisetti <siva@alphabiz.com.au>
2024-08-08 11:22:51 +0800

    I310 - Parth 2: Example 2.1.2: Using Two GetList() Calls (SOAP)

---
+++ MyStoreIntegration/Integration/PerforanceOptimization.cs
@@ -0,0 +1,111 @@
+﻿using MyStoreIntegration.Default;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStoreIntegration.Integration {
+	internal class PerforanceOptimization {
+		//Retrieving the list of sales orders of a customer
+		public static void ExportSalesOrders(DefaultSoapClient soapClient) {
+			Console.WriteLine("Getting the list of sales orders of a customer...");
+
+			//Customer data
+			string customerID = "C000000003";
+
+			//Specify the customerID of a customer whose sales orders should be
+			//exported and the fields to be returned
+			SalesOrder ordersToBeFound = new SalesOrder {
+				//Return only the specified values
+				ReturnBehavior = ReturnBehavior.OnlySpecified,
+
+				//Specify the customer whose sales order should be returned
+				CustomerID = new StringSearch { Value = customerID },
+
+				//Specify the fields of the entity and details to be returned
+				OrderType = new StringReturn(),
+				OrderNbr = new StringReturn(),
+				CustomerOrder = new StringReturn(),
+				Date = new DateTimeReturn(),
+				OrderedQty = new DecimalReturn(),
+				OrderTotal = new DecimalReturn(),
+				Details = new SalesOrderDetail[] {
+					new SalesOrderDetail {
+						InventoryID = new StringReturn(),
+						OrderQty = new DecimalReturn(),
+						UnitPrice = new DecimalReturn(),
+					}
+				}
+			};
+
+			//Get the list of sales orders with details
+			Entity[] soList = soapClient.GetList(ordersToBeFound);
+
+			//Save the results to a CSV file
+			using (StreamWriter file = new StreamWriter(string.Format(@"SalesOrderDetails_Customer_{0}.csv", customerID))) {
+				//Add headers to the file
+				file.WriteLine("OrderType;OrderNbr;CustomerID;CustomerOrder;Date;" +
+					"OrderedQty;OrderTotal;InventoryID;OrderQty;UnitPrice;");
+
+				//Write the values for each sales order
+				foreach (SalesOrder salesOrder in soList) { 
+					foreach (SalesOrderDetail detail in salesOrder.Details) {
+						file.WriteLine(string.Format(
+							"{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};",
+							//Document summary
+							salesOrder.OrderType.Value,
+							salesOrder.OrderNbr.Value,
+							salesOrder.CustomerID.Value,
+							salesOrder.CustomerOrder.Value,
+							salesOrder.Date.Value,
+							salesOrder.OrderedQty.Value,
+							salesOrder.OrderTotal.Value,
+							detail.InventoryID.Value,
+							detail.OpenQty.Value,
+							detail.UnitCost.Value));
+					}
+				}
+			}
+
+			//Specify the fields in shipments to be returned
+			ordersToBeFound = new SalesOrder {
+				ReturnBehavior = ReturnBehavior.OnlySpecified,
+
+				CustomerID = new StringSearch { Value = customerID },
+
+				OrderType = new StringReturn(),
+				OrderNbr = new StringReturn(),
+				Shipments = new SalesOrderShipment[] {
+					new SalesOrderShipment {
+						ShipmentNbr = new StringReturn(),
+						InvoiceNbr = new StringReturn(),
+					}
+				}
+			};
+
+			//Get the list of sales orders of the customer
+			soList = soapClient.GetList(ordersToBeFound);
+
+			//Save results to a CSV file
+			using (StreamWriter file = new StreamWriter(string.Format(@"SalesOrderShipments_Customer_{0}.csv", customerID))) {
+				//Add headers to the file
+				file.WriteLine("OrderType;OrderNbr;ShipmentNbr;InvoiceNbr;");
+
+				//Write the values for each sales order
+				foreach (SalesOrder salesOrder in soList) {
+					foreach (SalesOrderShipment shipment in salesOrder.Shipments) {
+						file.WriteLine(string.Format("{0};{1};{2};{3};",
+							//Document summary
+							salesOrder.OrderType.Value,
+							salesOrder.OrderNbr.Value,
+							shipment.ShipmentNbr.Value,
+							shipment.InvoiceNbr.Value
+						));
+					}
+				}
+			};
+		}
+	}
+}
---
+++ MyStoreIntegration/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using IdentityModel.Client;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Channels;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using MyStoreIntegration.Default;
+using MyStoreIntegration.Integration;
+
+namespace MyStoreIntegration {
+	internal class Program {
+		static void Main(string[] args) {
+			//This code is necessary only if you connect to the website
+			//through the HTTPS connection and
+			//you need to use cusom validation of an SSL certificate
+			//(for example, if the website uses a self-signed certificate).
+			ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateRemoteCertificate);
+
+			//Discover the token endpoint
+			DiscoveryClient discoveryClient = new DiscoveryClient(Properties.Settings.Default.IdentityEndpoint);
+			DiscoveryResponse discoveryResponse = discoveryClient.GetAsync().Result;
+
+			//Obtain and use the access token
+			using (TokenClient tokenClient = new TokenClient(discoveryResponse.TokenEndpoint, Properties.Settings.Default.ClientID, Properties.Settings.Default.ClientSecret)) {
+				tokenClient.BasicAuthenticationHeaderStyle = BasicAuthenticationHeaderStyle.Rfc2617;
+				var result = tokenClient.RequestResourceOwnerPasswordAsync(
+					Properties.Settings.Default.Username,
+					Properties.Settings.Default.Password,
+					Properties.Settings.Default.Scope
+				).Result;
+
+				string accessToken = result.AccessToken;
+
+				//Using the Default/18.200.001 endpoint
+				using (DefaultSoapClient soapClient = new DefaultSoapClient()) {
+					try {
+						soapClient.Endpoint.EndpointBehaviors.Add(new AccessTokenAdderBehavior(accessToken));
+						//You will add the integration code here
+						PerforanceOptimization.ExportSalesOrders(soapClient);
+					}
+					catch (Exception ex) { 
+						Console.WriteLine(ex);
+						Console.WriteLine();
+						Console.WriteLine("Press any key to continue");
+						Console.ReadLine();
+					} 
+					finally {
+						//Signu out from MYOB Advanced
+						soapClient.Logout();
+					}
+				}
+			}
+		}
+
+		//A supplymentary class that adds the access token
+		//to each request to the service
+		private class AccessTokenAdderBehavior : IEndpointBehavior, IClientMessageInspector {
+			private readonly string _accessToken;
+
+			public AccessTokenAdderBehavior(string accessToken) {
+				_accessToken = accessToken;
+			}
+
+			void IEndpointBehavior.Validate(ServiceEndpoint endpoint) { }
+
+			void IEndpointBehavior.AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+
+			void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher) { }
+
+			void IEndpointBehavior.ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime) => clientRuntime.ClientMessageInspectors.Add(this);
+
+			object IClientMessageInspector.BeforeSendRequest(ref Message request, IClientChannel channel) {
+				var httpRequestMessageProperty = new HttpRequestMessageProperty();
+				httpRequestMessageProperty.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + _accessToken);
+				request.Properties.Remove(HttpRequestMessageProperty.Name);
+				request.Properties.Add(HttpRequestMessageProperty.Name, httpRequestMessageProperty);
+				return null;
+			}
+
+			void IClientMessageInspector.AfterReceiveReply(ref Message reply, object correlationState) {
+			}
+		}
+
+		//A callback, which is used to validate the certificate of
+		//an MYOB Advanced website in an SSL conversation
+		private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors) {
+			//For simplicity, this callback always returns true.
+			//In a real integration application, you much check the SSL
+			//certificate here.
+			return true;
+		}
+	}
+}
